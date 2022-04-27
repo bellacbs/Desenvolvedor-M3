1b5a1b6a24df73faba84eea4da6d32a09ac5e4ce
bellacbs <bellacbrandao@gmail.com>
2022-04-27 00:06:28 -0300

    corrigindo bug do carrinho

--- src/global/GlobalState.js
+++ src/global/GlobalState.js
@@ -1,9 +1,26 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { GlobalStateContext } from "./GlobalStateContext"
 
 const GlobalState = (props) => {
     const [cartProducts, setCartProducts] = useState([])
 
+    const getCardProducts = () => {
+        const cart = JSON.parse(localStorage.getItem("cart"))
+
+        if (!cart) {
+            const newCart = []
+            localStorage.setItem("cart", JSON.stringify(newCart))
+            setCartProducts(newCart)
+        } else {
+            const newCart = cart
+            localStorage.setItem("cart", JSON.stringify(newCart))
+            setCartProducts(newCart)
+        }
+    }
+
+    useEffect(() => {
+        getCardProducts()
+    }, [])
 
     return (
         <GlobalStateContext.Provider value={{ cartProducts, setCartProducts }}>
